5400c59e3bcc00465491045e282ce9d7f0d79a65
NecmiK <necmik@gmail.com>
2021-02-07 00:54:27 +0300

    Lambdas on Stream Operations

---
+++ src/java11/streamlambdas/StreamGrouping.java
@@ -0,0 +1,143 @@
+package java11.streamlambdas;
+
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 8: Lambda Operations on Stream
+Topic: Stream Grouping
+*/
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamGrouping {
+    public static void main(String[] args) {
+
+        // Print one Pet object
+        System.out.println(new Pet());
+
+        // Create a randomly generated list of 5000 pets.
+        List<Pet> petPopulation = Stream.generate(Pet::new)
+                .limit(5000)
+                .collect(Collectors.toList());
+
+        // Count the number of Dogs
+        System.out.println("Number of Dogs = " +
+                petPopulation.stream()
+                        .filter((s) -> s.getType() == "Dog")
+                        .count());
+
+        System.out.println("--- Counts by Pet Type: ---");
+        petPopulation.stream()
+                // Collects to Map<String,Long> where key=pet type
+                .collect(Collectors.groupingBy(Pet::getType,
+                        Collectors.counting()))
+                // Create a stream from Map.entrySet()
+                .entrySet().stream()
+                // Sort stream by pet type (key for Map)
+                .sorted((s, t) -> s.getKey().compareTo(t.getKey()))
+                // Print element: a key/value pair
+                .forEach(System.out::println);
+
+        System.out.println("--- Average Age by State, PetType: ---");
+        Map<List, Double> petMaps = petPopulation.stream()
+                // Collect data into a Map<List,Double>
+                .collect(
+                        Collectors.groupingBy(
+                                // Grouping by list of attributes
+                                p -> Arrays.asList(
+                                        p.getState(), p.getType()),
+                                // Get average age of pet
+                                Collectors.averagingInt(Pet::getAge)));
+
+        // Can get very specific about requesting information from Map
+        System.out.println(String.format(
+                "Average age of a dog in CO = %.2f ",
+                petMaps.get(Arrays.asList("CO", "Dog"))));
+
+        System.out.println("--- Counts by State, PetType: ---");
+        petPopulation.stream()
+                // Collect data into a Map<List,Long>
+                .collect(
+                        Collectors.groupingBy(
+                                // Grouping by list of attributes
+                                p -> Arrays.asList(p.getState(), p.getType()),
+                                // Count pets by state, type
+                                Collectors.counting()))
+                // Create a stream from Map.entrySet()
+                .entrySet().stream()
+                // Sort first by state, then type, each part of key which is List
+                .sorted((s, t) -> (s.getKey().get(0) + s.getKey().get(1))
+                        .compareTo(t.getKey().get(0) + t.getKey().get(1)))
+                // Limit data to just Cat and Dog
+                .filter((s) -> (s.getKey().get(1).equals("Cat") ||
+                        s.getKey().get(1).equals("Dog")))
+                // Print each mapped entry
+                .forEach(System.out::println);
+
+        // Using .groupingBy with 3 arguments, second argument is Supplier
+        // Count Pet Population by each Pet Type
+        Map<String, Long> typeCounts = petPopulation.stream()
+                // Collects to Map<String,Long> where key=pet type
+                .collect(Collectors.groupingBy(Pet::getType,
+                        // Want a treeMap so map is ordered
+                        TreeMap::new,
+                        Collectors.counting()));
+        System.out.println("----- Ordered Mapping now ----- ");
+        System.out.println(typeCounts);
+
+        System.out.println("----- Creating Nested Collections ----- ");
+        Map<String, Map<String, Long>> TwoDMap = petPopulation.stream()
+                // Collect data into a Map<List,Double>
+                .collect(Collectors.groupingBy(
+                        // Grouping by State first
+                        Pet::getState,
+                        // Specify Sorted Map
+                        TreeMap::new,
+                        // Get average age of pet
+                        Collectors.groupingBy(
+                                // Grouping by Pet Type next
+                                Pet::getType,
+                                // Specify Sorted Map
+                                TreeMap::new,
+                                Collectors.counting()
+                        )));
+
+        TwoDMap.entrySet().forEach(System.out::println);
+
+        //Creates bifurcated population
+        Map<Boolean, List<Pet>> dogsAndNotDogs =
+                petPopulation.stream().collect(
+                        // Map<Boolean,List<Pet>
+                        Collectors.partitioningBy(s -> s.getType().equals("Dog"))
+                );
+
+        System.out.println("Print first five dogs");
+        dogsAndNotDogs.entrySet().stream()
+                // Only want Dogs, so key is true
+                .filter((s) -> s.getKey())
+                // Flatten list of Pets to a Stream
+                .flatMap(s -> s.getValue().stream())
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println("Just print dog population by veterinary type");
+        petPopulation.stream().collect(
+                // Map<Boolean, Map<String,Long>
+                Collectors.partitioningBy(s -> s.getType().equals("Dog"),
+                        Collectors.groupingBy(
+                                // Grouping by Veterinary Practice
+                                Pet::getVet,
+                                // Specify Sorted Map
+                                TreeMap::new,
+                                Collectors.counting()))
+        )
+                .entrySet().stream()
+                .filter((s) -> s.getKey())
+                .forEach(System.out::println);
+    }
+}
---
+++ src/java11/streamlambdas/StreamsSorted.java
@@ -0,0 +1,82 @@
+package java11.streamlambdas;
+
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 8: Lambda Operations on Stream
+Topic: Sorting Streams
+*/
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+class Guest implements Comparable<Guest>{
+    protected String first;
+    protected String last;
+
+    // Constructor
+    Guest(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String toString() {
+        return this.last + ", " + this.first;
+    }
+
+    public int compareTo(Guest o) {
+        return last.compareToIgnoreCase(o.last);
+    }
+
+    public static int sortByFirst(Guest g1, Guest g2) {
+        return g1.first.compareToIgnoreCase(g2.first);
+    }
+}
+
+public class StreamsSorted {
+
+    public static void main(String[] args) {
+
+        // Test data
+        List<String> initialData = List.<String>of("One", "Two", "Three",
+                "Four", "Five", "Six", "Abc");
+
+        // HashSet is unordered
+        Set<String> unOrderedSet = new HashSet<String>(initialData);
+
+        System.out.println("---- UnOrdered Stream ");
+        // Stream<Stream> created, Print each element
+        initialData.stream()
+                // print each element
+                .forEach((s) -> System.out.print(s + " "));
+
+        System.out.println("\n---- Using sorted operation on Stream");
+        initialData.stream()
+                // Uses Natural Order, alphabetical
+                .sorted()
+                // print each element
+                .forEach((s) -> System.out.print(s + " "));
+
+        System.out.println("\n---- Using sorted with Stream of Guest");
+
+        Stream.of(new Guest("Ann", "Jones"),
+                new Guest("Bob", "Smith"),
+                new Guest("Carol", "Green"))
+                .sorted(Guest::sortByFirst)
+                .forEach((s) -> System.out.println(s.first + " " + s.last));
+
+        System.out.println("\n---- Stream with multiple sorts/peeks");
+        initialData.stream()
+                // Pass a Comparator that sorts by length of String value
+                .sorted((s, t) -> s.length() - t.length())
+                .peek((s) -> System.out.println(s + " "))
+                // Uses Natural Order, alphabetical, same as sorted() with
+                // no argument
+                .sorted(Comparator.reverseOrder())
+                .forEach((s) -> System.out.print(s + " "));
+
+    }
+}
